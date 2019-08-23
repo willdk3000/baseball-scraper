c0ccaadbb8195e5a590e20696a5daa3e3a98a78b
willdk3000 <w.doucetk@gmail.com>
2019-08-22 21:43:39 -0400

    ajout resultat reel

--- client/src/components/Matchups.js
+++ client/src/components/Matchups.js
@@ -17,40 +17,42 @@ class Matchup extends Component {
                                             <div className='row'>
                                                 <div className='col-lg-6'>
                                                     <img className='hometeamlogo' src={game.team_logo} alt="teamlogo"></img>
-                                                    <p>Home team</p>
+                                                    <p>Home team</p><br />
+                                                    <p>{game.outcome ? game.outcome : ''}</p>
                                                     <table className="table table-bordered table-striped text-center">
                                                         <tbody>
                                                             <tr>
                                                                 <td>My odds</td>
                                                                 <td style={{
-                                                                        backgroundColor: game.my_outcome === 'win' ? "#E1FFE1" : "#FFE2E2"
-                                                                    }}>{game.my_odds ? game.my_odds.toFixed(2) : 'no odds calculated'}</td>
+                                                                    backgroundColor: game.my_outcome === 'win' ? "#E1FFE1" : "#FFE2E2"
+                                                                }}>{game.my_odds ? game.my_odds.toFixed(2) : 'no odds calculated'}</td>
                                                             </tr>
                                                             <tr>
                                                                 <td>Moj odds</td>
                                                                 <td style={{
-                                                                        backgroundColor: game.moj_outcome === 'win' ? "#E1FFE1" : "#FFE2E2"
-                                                                    }}>{game.moj_odds ? game.moj_odds : 'no odds calculated'}</td>
+                                                                    backgroundColor: game.moj_outcome === 'win' ? "#E1FFE1" : "#FFE2E2"
+                                                                }}>{game.moj_odds ? game.moj_odds : 'no odds calculated'}</td>
                                                             </tr>
                                                         </tbody>
                                                     </table>
                                                 </div>
                                                 <div className='col-lg-6'>
                                                     <img className='awayteamlogo' src={game.opponent_logo} alt="opponentlogo"></img>
-                                                    <p>Away team</p>
+                                                    <p>Away team</p><br />
+                                                    <p>{game.outcome ? game.outcome === 'win' ? 'loss' : 'win' : ''}</p>
                                                     <table className="table table-bordered table-striped text-center">
                                                         <tbody>
                                                             <tr>
                                                                 <td>My odds</td>
                                                                 <td style={{
-                                                                        backgroundColor: game.opponent_my_outcome === 'win' ? "#E1FFE1" : "#FFE2E2"
-                                                                    }}>{game.opponent_my_odds ? game.opponent_my_odds.toFixed(2) : 'no odds calculated'}</td>
+                                                                    backgroundColor: game.opponent_my_outcome === 'win' ? "#E1FFE1" : "#FFE2E2"
+                                                                }}>{game.opponent_my_odds ? game.opponent_my_odds.toFixed(2) : 'no odds calculated'}</td>
                                                             </tr>
                                                             <tr>
                                                                 <td>Moj odds</td>
                                                                 <td style={{
-                                                                        backgroundColor: game.opponent_moj_outcome === 'win' ? "#E1FFE1" : "#FFE2E2"
-                                                                    }}>{game.opponent_moj_odds ? game.opponent_moj_odds : 'no odds calculated'}</td>
+                                                                    backgroundColor: game.opponent_moj_outcome === 'win' ? "#E1FFE1" : "#FFE2E2"
+                                                                }}>{game.opponent_moj_odds ? game.opponent_moj_odds : 'no odds calculated'}</td>
                                                             </tr>
                                                         </tbody>
                                                     </table>
